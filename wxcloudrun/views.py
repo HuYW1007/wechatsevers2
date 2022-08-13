4727abc3d51d7582ceac5579dbf1e56df7fcf99e
DESKTOP-S29GTP5\yw hu <805781805@qq.com>
2022-08-14 00:15:44 +0800

    尝试解决部署后post 500错误

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,29 +1,36 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
 from . import receive
 from . import reply
+from . import taobaoke
 
+
+@csrf_exempt
 def autoreply(request):
     try:
-        webData = request.body
-        print("Handle POST webData is: ", webData)
+        wxData = request.body
+        print("Handle POST wxData is: ", wxData)
 
-        recMsg = receive.parse_xml(webData)
+        recMsg = receive.parse_xml(wxData)
         if isinstance(recMsg, receive.Msg):
             toUser = recMsg.FromUserName
             fromUser = recMsg.ToUserName
             if recMsg.MsgType == 'text':
                 content = recMsg.Content.decode('utf-8')
+                content = taobaoke.TbkRetuMsg(content)
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
-                return replyMsg.send()
+                return  HttpResponse(replyMsg.send())
             if recMsg.MsgType == 'image':
                 mediaId = recMsg.MediaId
                 replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
-                return replyMsg.send()
+                return HttpResponse(replyMsg.send())
             else:
-                return reply.Msg().send()
+                return HttpResponse(reply.Msg().send())
         else:
             print("暂不处理")
             # return "success"
-            return reply.Msg().send()
+            return HttpResponse(reply.Msg().send())
 
     except Exception as e:
         print(e)
